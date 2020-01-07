c140b57c1b106336ffde3d347c2b71616e39e687
Joshaby <jose.azevedo@academico.ifpb.edu.br>
2020-01-07 11:15:33 -0300

    GrafoMap: adicionadas as funções verticesNaoAdj, haCiclo, haArestasParalelas, verticesIncidentes e ehCompleto, todas retiradas do filho GrafoMapNDir. Motivo foi o uso dessas funções em outros filhos de GrafoMap, com isso, não haverá repetição de código

--- Grafos/AbstractGrafos/src/main/java/br/edu/ifpb/GrafoMap.java
+++ Grafos/AbstractGrafos/src/main/java/br/edu/ifpb/GrafoMap.java
@@ -1,7 +1,6 @@
 package br.edu.ifpb;
 
-import java.util.Map;
-import java.util.TreeMap;
+import java.util.*;
 import java.util.stream.Collectors;
 import br.edu.ifpb.Aresta;
 
@@ -43,6 +42,48 @@ public abstract class GrafoMap extends Grafo {
             if (i.equals(a)) return true;
         }
         return false;
+    }
+    public String verticesNaoAdj() {
+        List<String> verticesList = new ArrayList<>();
+        for (String i : vertices) {
+            for (String j : vertices) {
+                Aresta a = new Aresta(i, j);
+                if (!temAresta(a) && !temAresta(new Aresta(a.getVerticeB(), a.getVerticeA())))
+                    verticesList.add(a.toString());
+            }
+        }
+        return String.join(", ", verticesList);
+    }
+    public boolean haCiclo() {
+        for (Aresta a : arestas.values()) {
+            if (a.getVerticeB().equals(a.getVerticeA())) return true;
+        }
+        return false;
+    }
+    public boolean haArestasParalelas() {
+        for (int i : arestas.keySet()) {
+            for (int j : arestas.keySet()) {
+                if (i != j) {
+                    if (arestas.get(i).equals(arestas.get(j))) return true;
+                }
+            }
+        }
+        return false;
+    }
+    public String verticesIncidentes(String vertice) {
+        List<String> vertices = new ArrayList<>();
+        for (Aresta a : arestas.values()) {
+            if (a.getVerticeA().equals(vertice)) vertices.add(a.getVerticeB());
+            else if (a.getVerticeB().equals(vertice)) vertices.add(a.getVerticeA());
+        }
+        return String.join(", ", vertices);
+    }
+    public boolean ehCompleto() {
+        for (String v : vertices) {
+            List<String> verticesV = Arrays.asList(verticesIncidentes(v).split(", "));
+            if (!(verticesV.size() >= vertices.size() - 1)) return false;
+        }
+        return true;
     }
 
     @Override
--- Grafos/ConcreteGrafos/src/main/java/br/edu/ifpb/GrafoMapNDir.java
+++ Grafos/ConcreteGrafos/src/main/java/br/edu/ifpb/GrafoMapNDir.java
@@ -9,47 +9,4 @@ import java.util.stream.Collectors;
 public class GrafoMapNDir extends GrafoMap {
     public GrafoMapNDir(String[] vertices) { super(vertices); }
     public GrafoMapNDir(String[] vertices, Map<Integer, Aresta> arestas) { super(vertices, arestas); }
-
-    public String verticesNaoAdj() {
-        List<String> verticesList = new ArrayList<>();
-        for (String i : vertices) {
-            for (String j : vertices) {
-                Aresta a = new Aresta(i, j);
-                if (!temAresta(a) && !temAresta(new Aresta(a.getVerticeB(), a.getVerticeA())))
-                    verticesList.add(a.toString());
-            }
-        }
-        return String.join(", ", verticesList);
-    }
-    public boolean haCiclo() {
-        for (Aresta a : arestas.values()) {
-            if (a.getVerticeB().equals(a.getVerticeA())) return true;
-        }
-        return false;
-    }
-    public boolean haArestasParalelas() {
-        for (int i : arestas.keySet()) {
-            for (int j : arestas.keySet()) {
-                if (i != j) {
-                    if (arestas.get(i).equals(arestas.get(j))) return true;
-                }
-            }
-        }
-        return false;
-    }
-    public String verticesIncidentes(String vertice) {
-        List<String> vertices = new ArrayList<>();
-        for (Aresta a : arestas.values()) {
-            if (a.getVerticeA().equals(vertice)) vertices.add(a.getVerticeB());
-            else if (a.getVerticeB().equals(vertice)) vertices.add(a.getVerticeA());
-        }
-        return String.join(", ", vertices);
-    }
-    public boolean ehCompleto() {
-        for (String v : vertices) {
-            List<String> verticesV = Arrays.asList(verticesIncidentes(v).split(", "));
-            if (!(verticesV.size() >= vertices.size() - 1)) return false;
-        }
-        return true;
-    }
 }
